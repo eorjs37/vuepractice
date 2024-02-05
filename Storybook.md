cdaf93627207e37ef5e20b2c579b93a833828545
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-02-06 08:37:22 +0900

    fix : vue story진행중

--- Storybook.md
+++ Storybook.md
@@ -12,3 +12,9 @@ What do you want to generate?
 What storybook version do you want? (Please specify semver range) (^6.4.3) 
 
 ```
+
+
+## addon-actions
+```
+어떤 함수를 호출했는지와, 인자를 알려주는 정보이다.
+```
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list-item">
+        <label for="title">
+            <input type="text" readonly :value="task.title" id="title" name="title" />
+        </label>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        task: {
+            type: Object,
+            required: true,
+            default: () => ({ id: '', state: '', title: '' }),
+            validator: (task) => ['id', 'state', 'title'].every((key) => key in task)
+        }
+    },
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
